12af11eb14f44a462c7c13c2714c0efb70b243fe
Aswin Sivaraman .R <28544215+asivaramanr@users.noreply.github.com>
2021-08-28 02:29:31 +0530

    MELPA Stable with python dev enviornment

--- init.el
+++ init.el
@@ -1,73 +1,73 @@
 ;; .emacs.d/init.el
 
-
 ;; ===================================
 
 ;; MELPA Package Support
 
 ;; ===================================
 
-;; Enables basic packaging support
+;; Enables MELPA Stable  packaging support
 
 (require 'package)
 
-
-;; Adds the Melpa archive to the list of available repositories
-
 (add-to-list 'package-archives
-
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-
-;; Initializes the package infrastructure
-
 (package-initialize)
-
 
 ;; If there are no archived package contents, refresh them
 
 (when (not package-archive-contents)
-
   (package-refresh-contents))
 
-
 ;; Installs packages
-
-;;
 
 ;; myPackages contains a list of package names
 
 (defvar myPackages
 
-  '(better-defaults                 ;; Set up some better Emacs defaults
+  '(better-defaults               ;; Set up some better Emacs defaults
 
-    elpy                            ;; Emacs Lisp Python Environment
+    vscode-dark-plus-theme        ;; Theme
 
-    flycheck                        ;; On the fly syntax checking
+    projectile                    ;; open files within projects
 
-    py-autopep8                     ;; Run autopep8 on save
+    solaire-mode                  ;; solaire folder view
+   
+    auto-complete                 ;; auto-completion for codes
 
-    vscode-dark-plus-theme          ;; Theme
+    magit 			  ;; github
 
-    solaire-mode                    ;; solaire folder view
+    neotree                       ;; neotree side view
+
+    org-bullets                   ;; for org mode
+
+    elpy                          ;; Emacs Lisp Python Environment
+
+    flycheck                      ;; On the fly syntax checking
+
+    py-autopep8                   ;; Run autopep8 on save
+
+    python-black                     ;; Black formatting on save
+
+    docker-compose-mode           ;; docker-compose
+
+    dockerfile-mode               ;; docker file
+
+    yaml-mode                     ;; yaml mode
+
+    all-the-icons                 ;; icons for neotree
 
     )
 
   )
 
-
 ;; Scans the list in myPackages
 
-;; If the package listed is not already installed, install it
-
 (mapc #'(lambda (package)
-
           (unless (package-installed-p package)
-
             (package-install package)))
-
       myPackages)
-
 
 ;; ===================================
 
@@ -75,20 +75,95 @@
 
 ;; ===================================
 
+(setq message-log-max t)				         ;;message log
 
-(setq inhibit-startup-message t)    ;; Hide the startup message
+(add-to-list 'default-frame-alist '(fullscreen . maximized))     ;; Maximize window while startup
 
-(solaire-global-mode +1)            ;; Folder view
+(setq inhibit-startup-message t)    			         ;; Hide the startup message
 
-(load-theme 'vscode-dark-plus t)    ;; Visual Studio Code Theme 
+(global-linum-mode t)               			         ;; Enable line numbers globally
 
-(global-linum-mode t)               ;; Enable line numbers globally
+(setq linum-format "%4d  ")	    			 ;; Default indend after line number
 
-(add-hook 'window-setup-hook 'toggle-frame-maximized t) ;; Maximize window while startup
+(show-paren-mode 1)					         ;; shows pair brackets	
 
-(windmove-default-keybindings)      ;; Move windows by SHIFT + Arrow
+;;(global-hl-line-mode)        				         ;; Highlighting the Current Line
 
-(setq linum-format "%d ")	    ;; Default indend after line number		
+(load-theme 'vscode-dark-plus t)    			         ;; Visual Studio Code Theme 
+
+(solaire-global-mode +1)            			         ;; Folder view
+
+(ac-config-default)           				         ;; autocomplete
+
+ (yas-global-mode 1)                                              ;; Snippets for all languages
+
+(electric-pair-mode 1)                                            ;; Auto Brackets electric-pair-mode
+
+;; ===================================
+
+;; Default Directory
+
+;; ===================================
+
+(setq default-directory "C:\\Users\\AswinSivaramanR\\Documents\\github")
+(setq command-line-default-directory "C:\\Users\\AswinSivaramanR\\Documents\\github")
+
+;; ===================================
+
+;; projectile
+
+;; ===================================
+
+(projectile-mode +1)
+
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(setq projectile-indexing-method 'alien)
+(setq projectile-sort-order 'default)
+(setq projectile-enable-caching t)
+
+;; ===================================
+
+;; neotree
+
+;; ===================================
+
+(global-set-key [f8] 'neotree-toggle)
+
+(setq neo-autorefresh nil)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+;; Work with Projectile
+
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+(defun my-neotree-project-dir-toggle ()
+ (interactive)
+  (require 'neotree)
+  (let* ((filepath (buffer-file-name))
+         (project-dir
+          (with-demoted-errors
+              (cond
+               ((featurep 'projectile)
+                (projectile-project-root))
+               ((featurep 'find-file-in-project)
+                (ffip-project-root))
+               (t ;; Fall back to version control root.
+                (if filepath
+                    (vc-call-backend
+                     (vc-responsible-backend filepath) 'root filepath)
+                  nil)))))
+         (neo-smart-open t))
+
+    (if (and (fboundp 'neo-global--window-exists-p)
+             (neo-global--window-exists-p))
+        (neotree-hide)
+      (neotree-show)
+      (when project-dir
+        (neotree-dir project-dir))
+      (when filepath
+        (neotree-find filepath)))))
+
+(define-key global-map (kbd "M-e") 'my-neotree-project-dir-toggle)
 
 ;; ===================================
 
@@ -96,16 +171,71 @@
 
 ;; ===================================
 
-(setq org-support-shift-select t)
 (require 'org)
-(setq org-todo-keywords
-  '((sequence "TODO" "IN-PROGRESS" "WAITING" "VERIFY" "ISSUE" "TESTING" "|" "COMPLETED" "FIXED" )))
 
-;; Color for TODO's
+;; Orgmode Agenda
+
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-include-diary t)
+
+;; Default Directories
+
+  (setq org-directory (expand-file-name "C:\\Users\\AswinSivaramanR\\Documents\\github\\org"))
+  (setq org-agenda-files '("C:\\Users\\AswinSivaramanR\\Documents\\github\\org"))
+
+;; Agenda style
+
+(setq org-agenda-breadcrumbs-separator " ❱ "
+      org-agenda-current-time-string "⏰ ┈┈┈┈┈┈┈┈┈┈┈ now"
+      org-agenda-time-grid '((weekly today require-timed)
+                             (800 1000 1200 1400 1600 1800 2000)
+                             "---" "┈┈┈┈┈┈┈┈┈┈┈┈┈")
+      org-agenda-prefix-format '((agenda . "%i %-12:c%?-12t%b% s")
+                                 (todo . " %i %-12:c")
+                                 (tags . " %i %-12:c")
+                                 (search . " %i %-12:c")))
+
+(setq org-agenda-format-date (lambda (date) (concat "\n" (make-string (window-width) 9472)
+                                                    "\n"
+                                                    (org-agenda-format-date-aligned date))))
+(setq org-cycle-separator-lines 2)
+(require 'all-the-icons)
+(setq org-agenda-category-icon-alist
+      `(("Work" ,(list (all-the-icons-faicon "cogs")) nil nil :ascent center)
+        ("Personal" ,(list (all-the-icons-material "person")) nil nil :ascent center)
+        ("Calendar" ,(list (all-the-icons-faicon "calendar")) nil nil :ascent center)
+        ("Reading" ,(list (all-the-icons-faicon "book")) nil nil :ascent center)))
+
+
+;; Keyword sequence
+
+(setq org-todo-keywords
+  '(
+    (sequence "TODO(t)" "STARTED(s)" "NEXT(n)" "WAITING(w)" "CANCELLED(c)" "REPEAT(r)"
+              "HOLD(h)" "VERIFY(v)" "|" "DONE(d)" "DELEGATE(d)") 
+    (sequence "BUG(b)" "ISSUE(i)" "TESTING(t)" "|" "FIXED(f)")
+   
+   ))
+
+;; Faces
+
 (setq org-todo-keyword-faces
- '(("TODO" . "yellow") ("IN-PROGRESS" . "orange") ("ISSUE" . "orange") ("TESTING" . "yellow") ("WAITING" . "magenta") 
- ("VERIFY" . "cyan") ("COMPLETED" . "green") ("FIXED" . "green"))
- )
+ '(("TODO" . "GoldenRod") 
+   ("STARTED" . "yellow") 
+   ("NEXT" . "IndianRed1") 
+   ("WAITING" . "coral")
+   ("CANCELLED"  . "amber")
+   ("REPEAT" . "red") 
+   ("HOLD" . "magenta")
+   ("VERIFY" . "cyan") 
+   ("BUG" . "red") 
+   ("ISSUE" . "orange") 
+   ("TESTING" . "yellow") 
+   ("FIXED" . "green")
+   ("DELEGATE" . "forest green")  
+   ("DONE" . "green"))
+  )
 
 ;; Orgmode Bullets
 
@@ -113,25 +243,19 @@
 ;; (setq org-bullets-bullet-list '("☯" "○" "✸" "✿" "~"))
  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-;; Org-mode Priority color
+;; Orgmode priority color
 
 (setq org-priority-faces '((?A . (:foreground "red" :weight 'bold))
                            (?B . (:foreground "yellow"))
                            (?C . (:foreground "green"))))
 
-;; ===================================
-
-;; Default Directory Windows ONLY
-
-;; ===================================
-
-(setq default-directory "C:/Users/AswinSivaramanR/Documents")
-
-(setq command-line-default-directory "C:/Users/AswinSivaramanR/Documents")
+;; ====================================
+;; Development Setup
+;; ====================================
 
 ;; ====================================
 
-;; Development Setup
+;; Python
 
 ;; ====================================
 
@@ -139,39 +263,60 @@
 
 (elpy-enable)
 
-
 ;; Enable Flycheck
 
 (when (require 'flycheck nil t)
-
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
+
+(require 'yasnippet)
 
 ;; Enable autopep8
 
 (require 'py-autopep8)
-
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+;; ===================================
+
+;; yaml-mode
+
+;; ===================================
+
+(require 'yaml-mode)
+    (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+
+
+(add-hook 'yaml-mode-hook
+      '(lambda ()
+        (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; ===================================
+
+;; Docker
+
+;; ===================================
+
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+(require 'docker-compose-mode)
 
 ;; ===================================
 
 ;; User-Defined init.el ends here
 
 ;; ===================================
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(better-defaults)))
+ '(package-selected-packages
+   '(company-jedi all-the-icons dockerfile-mode docker-compose-mode vscode-dark-plus-theme solaire-mode py-autopep8 projectile org-bullets neotree magit flycheck elpy better-defaults auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
